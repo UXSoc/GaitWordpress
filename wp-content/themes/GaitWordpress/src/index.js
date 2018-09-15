9841ed32cb02f19c568db4ce3847d67bdd722992
Michael Pollind <polli104@mail.chapman.edu>
2018-09-14 23:04:44 -0700

    configured webpack build

---
+++ wp-content/themes/GaitWordpress/src/index.js
@@ -0,0 +1,4 @@
+import './scss/main.scss';
+import './js/customizer'
+import './js/navigation'
+import './js/skip-link-focus-fix'
---
+++ wp-content/themes/GaitWordpress/webpack.config.js
@@ -0,0 +1,60 @@
+// webpack v4
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                use: "url-loader?limit=100000"
+            }
+        ]
+    },
+    plugins: [
+        new ManifestPlugin(),
+        new CleanWebpackPlugin('dist', {}),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new WebpackMd5Hash(),
+        new StyleLintPlugin({
+            configFile: './stylelint.config.js',
+            files: './src/scss/*.scss',
+            syntax: 'scss'
+        })
+    ]
+};
